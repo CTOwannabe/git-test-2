72b7bdccefc03c42299cd0f05a9be59a4d2414ec
CTOwannabe <agentcs1509@gmial.com>
2024-05-09 21:40:06 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Halo Dek</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam porro atque quaerat reprehenderit perferendis? Repudiandae, quo excepturi. Quaerat ducimus ea, repellat repellendus ex, fugit consequatur libero possimus reiciendis culpa assumenda?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio illo ducimus voluptatibus sequi ea cupiditate autem, odit alias voluptate velit consequuntur neque debitis commodi. Magni repellendus inventore fugit eaque natus.</p>
+</body>
+</html>
